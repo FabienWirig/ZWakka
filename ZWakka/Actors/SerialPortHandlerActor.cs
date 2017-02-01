c3d9cfe30061900b38614eb31567009e88cb34bd
FabienW <fabien.wirig@gmail.com>
2017-02-02 00:04:26 +0100

    Poc

---
+++ ZWakka/Actors/SerialPortHandlerActor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using System.IO.Ports;
+
+namespace ZWakka.Actors
+{
+    public class SerialPortHandlerActor : ReceiveActor, IWithUnboundedStash
+    {
+        private readonly SerialPort _serialPort;
+        public IStash Stash { get; set; }
+
+        #region Messages
+        public class MessageReceived
+        {
+            public MessageReceived(byte[] message)
+            {
+                Message = message;
+            }
+
+            public byte[] Message { get; private set; }
+        }
+
+        public class SendMessage
+        {
+            public SendMessage(byte[] message)
+            {
+                Message = message;
+            }
+
+            public byte[] Message { get; private set; }
+        }
+
+        public class AckReceived {}
+
+        #endregion
+
+        public SerialPortHandlerActor(SerialPort serialPort)
+        {
+            _serialPort = serialPort;
+            NormalBehaviour();
+        }
+
+        private void NormalBehaviour()
+        {
+            Receive<MessageReceived>(mr => HandleMessageReceived(mr));
+            Receive<SendMessage>(sm =>
+            {
+                HandleSendMessage(sm);
+                Become(WaitingAckBehaviourBehaviour);
+            });
+            Receive<AckReceived>(sm => {
+                Console.WriteLine("unexpected ack");
+            });
+        }
+
+        private void WaitingAckBehaviourBehaviour()
+        {
+            Receive<MessageReceived>(mr => Stash.Stash());
+            Receive<SendMessage>(sm => Stash.Stash());
+            Receive<AckReceived>(sm => {
+                Become(NormalBehaviour);
+                Stash.UnstashAll();
+            });
+        }
+
+        private void HandleMessageReceived(MessageReceived messageReceived)
+        {
+            Console.WriteLine($"Message received : {getDisplayableString(messageReceived.Message)}");
+
+            //TODO : route messages
+        }
+
+        private void HandleSendMessage(SendMessage sendMessage)
+        {
+            var message = new List<byte>(sendMessage.Message);
+            message.Add(GenerateChecksum(message));
+
+            Console.WriteLine($"Message sent : {getDisplayableString(message)}");
+
+            _serialPort.Write(message.ToArray(), 0, message.Count);
+        }
+
+        private string getDisplayableString(IEnumerable<byte> message)
+        {
+            return string.Join(" ", message.Select(x => $"{x:x2}"));
+        }
+
+        private byte GenerateChecksum(List<byte> data)
+        {
+            var result = data[1];
+            for (var i = 2; i < data.Count; i++)
+            {
+                result ^= data[i];
+            }
+            result = (byte)(~result);
+            return result;
+        }
+    }
+}
--- ZWakka/Program.cs
+++ ZWakka/Program.cs
@@ -1,15 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Akka.Actor;
+using ZWakka.Actors;
 
 namespace ZWakka
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static ActorSystem MyActorSystem;
+        private readonly Queue<byte> _readBuffer = new Queue<byte>();
+        private List<byte> _currentMessage = new List<byte>();
+        private readonly IActorRef _serialPortHandlerActor;
+
+        private static void Main(string[] args)
         {
+            var p = new Program();
+        }
+
+        public Program()
+        {
+            var serialPort = new SerialPort
+            {
+                PortName = "COM6",
+                BaudRate = 115200,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Handshake = Handshake.None,
+                DtrEnable = true,
+                RtsEnable = true,
+                NewLine = System.Environment.NewLine
+            };
+
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.Open();
+            
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+            _serialPortHandlerActor = MyActorSystem.ActorOf(Props.Create(() => new SerialPortHandlerActor(serialPort)));
+            _serialPortHandlerActor.Tell(new SerialPortHandlerActor.SendMessage(new byte[] { 0x01, 0x03, 0x00, 0x20 }));
+
+            MyActorSystem.WhenTerminated.Wait();
+            serialPort.Close();
+        }
+
+        private void TrameReceived(SerialPort sp, byte[] message)
+        {
+            sp.Write(new byte[] { 0x06 }, 0, 1);
+            _serialPortHandlerActor.Tell(new SerialPortHandlerActor.MessageReceived(message));
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var sp = (SerialPort) sender;
+            ReadBytes(sp).ToList().ForEach(x => _readBuffer.Enqueue(x));
+            while (_readBuffer.Count != 0)
+            {
+                ProcessReceivedBytes(sp);
+            }
+        }
+
+        private byte[] ReadBytes(SerialPort port)
+        {
+            byte[] result;
+            if (port.IsOpen)
+            {
+                result = new byte[port.BytesToRead];
+                if (result.Length > 0)
+                {
+                    port.Read(result, 0, result.Length);
+                }
+            }
+            else
+            {
+                result = new byte[0];
+            }
+            return result;
+        }
+
+        private void ProcessReceivedBytes(SerialPort sp)
+        {
+            if (_readBuffer.Count > 0) _currentMessage = BuildMessage(_readBuffer, _currentMessage);
+            
+            byte[] result;
+            if (_currentMessage.Count > 1 && _currentMessage.Count == _currentMessage[1] + 2)
+            {
+                result = _currentMessage.ToArray();
+            }
+            else if (_currentMessage.Count == 1 && (_currentMessage[0] == 0x06 || _currentMessage[0] == 0x15))
+            {
+                result = _currentMessage.ToArray();
+            }
+            else
+            {
+                result = new byte[0];
+            }
+            
+            if (result.Length != 0)
+            {
+                _currentMessage = new List<byte>();
+
+                if (result[0] == 0x15) Console.WriteLine("Nack received !!!");
+                else if (result[0] == 0x06)
+                {
+                    _serialPortHandlerActor.Tell(new SerialPortHandlerActor.AckReceived());
+                } else
+                {
+                    TrameReceived(sp, result);
+                }
+            }
+        }
+        private List<byte> BuildMessage(Queue<byte> readBufer, List<byte> currentMessage)
+        {
+            if (currentMessage.Count == 0)
+            {
+                currentMessage.Add(readBufer.Dequeue());
+            }
+            
+            if (currentMessage.First() == 0x01)
+            {
+                if (currentMessage.Count == 1 && readBufer.Count >= 1)
+                {
+                    currentMessage.Add(readBufer.Dequeue());
+                }
+                
+                for (var i = 0; i < readBufer.Count && currentMessage.Count != currentMessage[1] + 2; i++)
+                {
+                    currentMessage.Add(readBufer.Dequeue());
+                }
+            }
+            return currentMessage;
         }
     }
 }
